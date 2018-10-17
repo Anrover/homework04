c971ee429ad4d4293289ce5429058f605ba89909
MIkhail Karpenko <krosh347@gmail.com>
2018-10-17 21:22:23 +0500

    first commit

--- src/main/scala/fintech/homework04/Tree.scala
+++ src/main/scala/fintech/homework04/Tree.scala
@@ -1,21 +1,42 @@
 package fintech.homework04
+import scala.annotation.tailrec
 
 sealed trait Tree[+A]
 case class Leaf[A](value: A) extends Tree[A]
 case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
+object Branching extends Tree[Nothing]
 
 object Tree {
   // реализовать функцию fold
-  def fold[A,B](t: Tree[A])(f: A => B)(g: (B,B) => B): B = ???
+
+//not tailrec
+//  def fold[A,B](t: Tree[A])(f: A => B)(g: (B,B) => B): B = t match {
+//    case Leaf(value) => f(value)
+//    case Branch(left, right) => g(fold(left)(f)(g), fold(right)(f)(g))
+//  }
+
+  def fold[A,B](t: Tree[A])(f: A => B)(g: (B,B) => B): B = {
+    @tailrec
+    def rec(subtrees: List[Tree[A]], acc: List[B]): B = subtrees match {
+      case List() => acc.head
+      case head :: tail => head match {
+        case Leaf(value) => rec(tail, f(value) :: acc)
+        case Branch(left, right) => rec(left :: right :: Branching :: tail, acc)
+        case Branching => rec(tail, g(acc.tail.head, acc.head) :: acc.drop(2))
+      }
+    }
+    rec(List(t), List())
+  }
 
   // реализовать следующие функции через fold
 
-  def size[A](t: Tree[A]): Int = ???
+  def size[A](t: Tree[A]): Int = fold(t)(_ => 1)((x, y) => x + y)
 
-  def max(t: Tree[Int]): Int = ???
+  def max(t: Tree[Int]): Int = fold(t)(x => x)((x, y) => Math.max(x, y))
 
-  def depth[A](t: Tree[A]): Int = ???
+  def depth[A](t: Tree[A]): Int = fold(t)(_ => 0)((x, y) => 1 + Math.max(x, y))
 
   //здесь возможно придется прибегнуть к насильному указанию типа: Leaf(a): Tree[A]
-  def map[A,B](t: Tree[A])(f: A => B): Tree[B] = ???
+  def map[A,B](t: Tree[A])(f: A => B): Tree[B] =
+    fold(t)(x => Leaf(f(x)): Tree[B])((x, y) => Branch(x, y))
 }
--- src/test/scala/fintech/homework04/TreeSpec.scala
+++ src/test/scala/fintech/homework04/TreeSpec.scala
@@ -2,5 +2,56 @@ package fintech.homework04
 import org.scalatest.{FlatSpec, Matchers}
 
 class TreeSpec extends FlatSpec with Matchers {
-  ???
+  val tree1: Tree[Int] = Branch(
+    Branch(
+      Leaf(5),
+      Branch(
+        Leaf(6),
+        Leaf(7))),
+    Branch(
+      Branch(
+        Branch(
+          Leaf(1),
+          Leaf(2)),
+        Leaf(2)),
+      Branch(
+        Leaf(3),
+        Leaf(3))))
+  val tree2: Tree[Char] = Branch(Leaf('a'), Leaf('b'))
+  val emptyTree: Tree[Int] = Leaf(10)
+
+  "Tree" should "correct calculate size" in {
+    Tree.size(tree1) should be (8)
+    Tree.size(emptyTree) should be (1)
+    Tree.size(tree2) should be (2)
+  }
+  it should "correct calculate depth" in {
+    Tree.depth(tree1) should be (4)
+    Tree.depth(tree2) should be (1)
+    Tree.depth(emptyTree) should be (0)
+  }
+  it should "correct find max number" in {
+    Tree.max(tree1) should be (7)
+    Tree.max(emptyTree) should be (10)
+  }
+  it should "should correct apply the function to all elements" in {
+    val multiplyCharA: Int => String = x => "A" * x
+    val treeString1: Tree[String] = Branch(
+      Branch(
+        Leaf(multiplyCharA(5)),
+        Branch(
+          Leaf(multiplyCharA(6)),
+          Leaf(multiplyCharA(7)))),
+      Branch(
+        Branch(
+          Branch(
+            Leaf(multiplyCharA(1)),
+            Leaf(multiplyCharA(2))),
+          Leaf(multiplyCharA(2))),
+        Branch(
+          Leaf(multiplyCharA(3)),
+          Leaf(multiplyCharA(3)))))
+    Tree.map(tree1)(multiplyCharA) == treeString1 should be (true)
+    Tree.map(tree1)(x => multiplyCharA(x + 1)) == treeString1 should be (false)
+  }
 }
